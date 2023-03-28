4d5ab195166099d258cbb82af03e2cf7553a496b
andrei <janandreicarlos@gmail.com>
2023-03-28 13:12:05 +0800

    Hiiii adding Home screen sabi ni redd :>

--- lib/main.dart
+++ lib/main.dart
@@ -3,122 +3,258 @@ import 'package:tom_conn/first_time_use.dart';
 import 'package:tom_conn/login.dart';
 import 'package:tom_conn/loading_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        colorScheme: ThemeData()
-            .colorScheme
-            .copyWith(primary: Color.fromRGBO(254, 192, 15, 1)),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: OnboardingScreen(),
-      routes: {
-        '/login': (context) => Login(),
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      title: 'Header Example',
+      home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          leading: InkWell(
+            onTap: () {
+              _scaffoldKey.currentState!.openDrawer();
+            },
+            child: Icon(
+              Icons.menu,
+              color: Colors.white,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          actions: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'About',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Contact Us',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Support',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'FAQs',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Sign Out',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                child: Text(
+                  'Menu',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text('Item 3'),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/ust_main_building.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Expanded(
+              child: GridView.count(crossAxisCount: 4, children: <Widget>[
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+              ]),
+            ),
+            Container(
+              color: Colors.black,
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Copyright © 2023',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
